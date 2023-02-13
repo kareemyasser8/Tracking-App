4474c317d6ef0231e0cbc245e6d3fd177ff48b24
kareemyasser8 <kareem.yasserr@gmail.com>
2023-02-13 21:36:01 +0200

    adding header Access-Control-Allow-Origin

--- tracking-app/src/app/services/track.service.ts
+++ tracking-app/src/app/services/track.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +7,16 @@ import { HttpClient } from '@angular/common/http';
 export class TrackService {
 
   private url = 'https://tracking.bosta.co/shipments/track'
-
-  constructor(private http: HttpClient) {
-
-  }
+  constructor(private http: HttpClient) {}
 
   getTrackInfo(id : string){
+    let headers = new HttpHeaders()
+    .set('content-type', 'application/json')
+    .set('Access-Control-Allow-Origin', '*');
 
-    return this.http.get(this.url + '/' + id);
+    return this.http.get((this.url + '/' + id),{
+      headers: headers
+    });
   }
 }
 
